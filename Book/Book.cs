903bdeb6af7fad85d0a75b26c9c5b3d057145886
Mike Kargin <redmoonsaber@gmail.com>
2019-01-13 22:04:18 +0300

    First commit

---
+++ Book/Book.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book
+{
+    class Book
+    {
+        private class Authtor
+        {
+            public string Field { get; set; }
+
+            public void Show()
+            {
+                Field.ToString();
+            }
+        }
+
+        private class Title
+        {
+            public string Field { get; set; }
+            public void Show()
+            {
+                Field.ToString();
+            }
+        }
+        }
+
+        private class Content
+        {
+            public string Field { get; set; }
+
+            public void Show()
+            {
+                Field.ToString();
+            }
+        }
+
+    }
+}
